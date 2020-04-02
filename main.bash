57df990458e27f86d236659c36ba961636d4f81b
Dylan <dylan.hitt1@gmail.com>
2020-04-01 21:56:58 -0400

    Trap and pretty print error with line number

--- main.bash
+++ main.bash
@@ -2,17 +2,36 @@
 
 # entrypoint for cli based applications
 
-set -e
+
+set -o errexit
+set -o errtrace
+set -o functrace
+set -o pipefail
+
+export ENV_ROOT_PATH=$(pwd)
+
 cd "$(dirname "$0")"
 
 source util/util_index.bash
 
-_main() {
-    # Parse options
-    # set debug options
-    # select command to run
-    _text.log DEBUG "hello world"
+handle_error() {
     
+    # ANY CLEAN UP GOES HERE #
+    
+    local line_number=$1
+    local failed_command=$2
+    _text.log ERROR "Error occured on $line_number with command $failed_command"
+}
+
+trap 'handle_error $LINENO "$BASH_COMMAND"' ERR
+
+_main() {
+    echo hello | grep fool
 }
 
 _main
+
+
+
+
+
